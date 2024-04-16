c294c4a656a95ecc4f6e6360aa9deab47183348e
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-04-16 10:04:00 +0200

    update: updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
